08a4faac0e0bbce27b616dd99f1346f82626d2aa
Mufida Alakulju <mufidaalakulju@gmail.com>
2024-03-11 19:32:59 +0200

    corrected spelling mistakes

--- app/controllers/generatiom_controller.py
+++ app/controllers/generatiom_controller.py
@@ -9,16 +9,16 @@ client.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_response(query, ret_list):
     
-    references = "\n".join([f"Refrence Text: {ret[1]}, Refrence Doc Title: {ret[2]}" for ret in ret_list])
+    references = "\n".join([f"Reference Text: {ret[1]}, Reference Doc Title: {ret[2]}" for ret in ret_list])
 
     completion = client.chat.completions.create(
         model="gpt-4-turbo-preview",
         messages=[
             {
                 "role": "system",
-                "content": f'You are a 3GPP specialized assistant that anwers to the user only based on the provided reference.' 
-                            f'Let the user know if the answer cannot be found in the given reference. respond "I could not find an answer."'
-                            f'Here is the reference between brakets:[{references}].',
+                "content": f'You are a 3GPP specialized assistant that answers to the user only based on the provided reference.' 
+                            f'If the answer can not be found in the given reference. respond "I could not find an answer."'
+                            f'Here is the reference between brackets:[{references}].',
             },
             {"role": "user", "content": query},
         ],
